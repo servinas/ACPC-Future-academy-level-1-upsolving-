30d2ecdc60092eea9e208f058a611d6bc68f1b0f
Servinas Hassan <37937041+servinas@users.noreply.github.com>
2023-04-15 02:42:54 +0200

    Create C - 753.cpp

---
+++ sheet 1/C - 753.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    int x;
+    cin >> x;
+    if (x == 7 || x == 5 || x == 3)cout << "YES";
+    else cout << "NO";
+    return 0;
+}
