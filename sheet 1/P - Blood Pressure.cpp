5ac7de7d2ffbb169d3b705f28bba6d3547fc159c
Servinas Hassan <37937041+servinas@users.noreply.github.com>
2023-04-15 07:25:15 +0200

    Create P - Blood Pressure.cpp

---
+++ sheet 1/P - Blood Pressure.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    int a, b;
+    double c;
+    cin >> a >> b;
+    c = (double )(a - b) / 3 + b;
+    cout << fixed << setprecision(6);
+    cout<<c;
+    return 0;
+}
