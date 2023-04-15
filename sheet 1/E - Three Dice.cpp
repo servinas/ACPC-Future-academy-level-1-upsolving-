3080717fa7fcdbfaa4bd04b7fdcd91b673a27c44
Servinas Hassan <37937041+servinas@users.noreply.github.com>
2023-04-15 03:52:14 +0200

    Create E - Three Dice.cpp

---
+++ sheet 1/E - Three Dice.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    a=7-a;
+    b=7-b;
+    c=7-c;
+    int sum=a+b+c;
+    cout<<sum;
+    return 0;
+}
